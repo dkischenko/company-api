99b7c83b67b1966a6766683b83ac4c42d9993f03
dmytro_kishchenko <dmitry_kischenko@gmail.com>
2022-10-14 11:39:16 +0300

    fixes

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ XM Golang Exercise - v22.0.0
 docker run -it --rm \
 --name some-postgres \
 -e POSTGRES_PASSWORD=password \
--e POSTGRES_USER=postgresPOSTGRES_USER \
+-e POSTGRES_USER=postgres \
 -p 5432:5432 \
 -e PGDATA=/var/lib/postgresql/data/pgdata \
 -v ~/local-go-postgres:/var/lib/postgresql/data \
--- configs/config.go
+++ configs/config.go
@@ -3,7 +3,7 @@ package configs
 type Config struct {
 	AppHost            string `env:"HOST" envDefault:"127.0.0.1"`
 	AppPort            string `env:"PORT" envDefault:"9090"`
-	DatabaseDsn        string `env:"DATABASE_DSN" envDefault:"host=db user=postgres password=password dbname=postgres port=5432 sslmode=disable"`
+	DatabaseDsn        string `env:"DATABASE_DSN" envDefault:"host=localhost user=postgres password=password dbname=postgres port=5432 sslmode=disable"`
 	KafkaNetwork       string `env:"KAFKA_NETWORK" envDefault:"tcp"`
 	KafkaHost          string `env:"KAFKA_HOST" envDefault:"localhost"`
 	KafkaPort          string `env:"KAFKA_PORT" envDefault:"9092"`
